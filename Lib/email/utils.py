c67d8f9a3a6099ef37c6d68a0996f8ba6e10747d
Victor Stinner <vstinner@python.org>
2023-10-20 15:26:41 +0200

    gh-102988: email parseaddr() now rejects malformed address
    Detect email address parsing errors and return empty tuple to
    indicate the parsing error (old API). Add an optional 'strict'
    parameter to getaddresses() and parseaddr() functions. Patch by
    Thomas Dwyer.
    
    Co-Authored-By: Thomas Dwyer <github@tomd.tel>

--- Lib/email/utils.py
+++ Lib/email/utils.py
@@ -103,12 +103,70 @@ def formataddr(pair, charset='utf-8'):
     return address
 
 
+def _pre_parse_validation(email_header_fields):
+    accepted_values = []
+    for v in email_header_fields:
+        s = v.replace('\\(', '').replace('\\)', '')
+        if s.count('(') != s.count(')'):
+            v = "('', '')"
+        accepted_values.append(v)
 
-def getaddresses(fieldvalues):
-    """Return a list of (REALNAME, EMAIL) for each fieldvalue."""
-    all = COMMASPACE.join(str(v) for v in fieldvalues)
-    a = _AddressList(all)
-    return a.addresslist
+    return accepted_values
+
+
+def _post_parse_validation(parsed_email_header_tuples):
+    accepted_values = []
+    # The parser would have parsed a correctly formatted domain-literal
+    # The existence of an [ after parsing indicates a parsing failure
+    for v in parsed_email_header_tuples:
+        if '[' in v[1]:
+            v = ('', '')
+        accepted_values.append(v)
+
+    return accepted_values
+
+
+realname_comma_re = re.compile(r'"[^"]*,[^"]*"')
+
+def getaddresses(fieldvalues, *, strict=True):
+    """Return a list of (REALNAME, EMAIL) or ('','') for each fieldvalue.
+
+    When parsing fails for a fieldvalue, a 2-tuple of ('', '') is returned in
+    its place.
+
+    If the resulting list of parsed address is greater than number of
+    fieldvalues in the input list a parsing error has occurred, so a list
+    containing a single empty 2-tuple [('', '')] is returned in its place.
+    This is done to avoid invalid output.
+
+    Malformed input: getaddresses(['alice@example.com <bob@example.com>'])
+    Invalid output: [('', 'alice@example.com'), ('', 'bob@example.com')]
+    Safe output: [('', '')]
+
+    If strict is true, use a strict parser which rejects malformed inputs.
+    """
+    if strict:
+        fieldvalues = [str(v) for v in fieldvalues]
+        fieldvalues = _pre_parse_validation(fieldvalues)
+        all = COMMASPACE.join(v for v in fieldvalues)
+        a = _AddressList(all)
+        result = _post_parse_validation(a.addresslist)
+
+        # When a comma is used in the Real Name part it is not a deliminator
+        # So strip those out before counting the commas
+        n = 0
+        for v in fieldvalues:
+            v = realname_comma_re.sub('', v)
+            n += v.count(',') + 1
+
+        if len(result) != n:
+            return [('', '')]
+
+        return result
+    else:
+        all = COMMASPACE.join(str(v) for v in fieldvalues)
+        a = _AddressList(all)
+        return a.addresslist
 
 
 def _format_timetuple_and_zone(timetuple, zone):
@@ -207,17 +265,34 @@ def parsedate_to_datetime(data):
             tzinfo=datetime.timezone(datetime.timedelta(seconds=tz)))
 
 
-def parseaddr(addr):
+def parseaddr(addr, *, strict=True):
     """
     Parse addr into its constituent realname and email address parts.
 
     Return a tuple of realname and email address, unless the parse fails, in
     which case return a 2-tuple of ('', '').
+
+    If strict is true, use a strict parser which rejects malformed inputs.
     """
-    addrs = _AddressList(addr).addresslist
-    if not addrs:
-        return '', ''
-    return addrs[0]
+    if strict:
+        if isinstance(addr, list):
+            addr = addr[0]
+
+        if not isinstance(addr, str):
+            return ('', '')
+
+        addr = _pre_parse_validation([addr])[0]
+        addrs = _post_parse_validation(_AddressList(addr).addresslist)
+
+        if not addrs or len(addrs) > 1:
+            return ('', '')
+
+        return addrs[0]
+    else:
+        addrs = _AddressList(addr).addresslist
+        if not addrs:
+            return '', ''
+        return addrs[0]
 
 
 # rfc822.unquote() doesn't properly de-backslash-ify in Python pre-2.3.
